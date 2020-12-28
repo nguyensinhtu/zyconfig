e22afade34f416b0f01287c107757941443e65f9
nguyensinhtu <didaihocdaihoc@gmail.com>
2020-12-28 16:00:46 +0700

    add setup process

---
+++ setpy.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='zyconfig',
+      version='0.0.1',
+      description='another ini config with more utilities',
+      url='https://github.com/nguyensinhtu/zyconfig',
+      author='Tuns',
+      author_email='nguyensinhtuit97@gmail.com',
+      packages=['zyconfig'],
+      include_package_data=True,
+      zip_safe=False,
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 3',
+          'Topic :: Applications ::micro-services'
+      ])
